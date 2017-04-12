ccd41e85904078672a585114c536bb9194ae76f3
Aleksey Dvoryanskiy <advoryanskiy@gmail.com>
2017-04-12 13:04:29 +0400

    Initial Commit

---
+++ applauncher/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  applauncher
+//
+//  Created by Aleksey Dvoryanskiy on 4/12/17.
+//  Copyright © 2017 none. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ applauncher/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  applauncher
+//
+//  Created by Aleksey Dvoryanskiy on 4/12/17.
+//  Copyright © 2017 none. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
